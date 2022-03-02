28a7bc735c9f53046bd38fa9f649933de0f29643
OzcarDev <cdmx2023@amerike.edu.mx>
2022-03-01 22:08:50 -0600

    comit2

---
+++ laberinto/Assets/Christopher/Scrpts/objectInteract.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectInteract : MonoBehaviour
+{
+	GameObject objeto;
+	bool isTouchingWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+	private void OnTriggerEnter(Collider other)
+    {
+
+	    if (other.gameObject.tag == "Obstacle")
+        {
+		    isTouchingWall= true;
+		    objeto = other.gameObject;
+           
+        }
+
+
+    }
+    
+	private void OnTriggerExit(Collider other){
+		if (other.gameObject.tag == "Obstacle")
+		{
+			isTouchingWall = false;
+			objeto = null;
+           
+		}
+	}
+    
+	void Update(){
+		if ( isTouchingWall &&(Input.GetKey(KeyCode.H)))
+		{
+			Destroy(objeto);
+           
+		}
+	}
+     
+}
